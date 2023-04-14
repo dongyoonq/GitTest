e1b51e8968d78d15290124b8d91940260cc82300
윤진 소 <pms010506@gmail.com>
2023-04-14 21:19:01 +0900

    Feat: 11번 k개의 정렬된 배열에서 공통항목을 찾는 솔루션

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace GitTest
+﻿using System.Threading.Channels;
+
+namespace GitTest
 {
     internal class Program
     {
@@ -70,7 +72,58 @@
             int result = SumOfDigits(input);
             Console.WriteLine(result);
 
+
+            // 11번
+
+            Console.Write("첫 번째 배열의 크기를 입력하세요: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[n1];       // 크기가 n1인 배열 arr1 생성
+
+            Console.WriteLine("첫 번째 배열의 원소들을 입력하세요:");
+            for (int i = 0; i < n1; i++)
+            {
+                Console.Write($"arr1[{i}] = ");
+                arr1[i] = int.Parse(Console.ReadLine());      // arr1의 0번째부터 i번째 원소까지를 입력
+            }
+
+            Console.Write("두 번째 배열의 크기를 입력하세요: ");
+            int n2 = int.Parse(Console.ReadLine());
+            int[] arr2 = new int[n2];
+
+            Console.WriteLine("두 번째 배열의 원소들을 입력하세요:");
+            for (int i = 0; i < n2; i++)
+            {
+                Console.Write($"arr2[{i}] = ");
+                arr2[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("세 번째 배열의 크기를 입력하세요: ");
+            int n3 = int.Parse(Console.ReadLine());
+            int[] arr3 = new int[n3];
+
+            Console.WriteLine("세 번째 배열의 원소들을 입력하세요:");
+            for (int i = 0; i < n3; i++)
+            {
+                Console.Write($"arr3[{i}] = ");
+                arr3[i] = int.Parse(Console.ReadLine());
+            }
+
+            int[] commonItems = FindCommonItems(arr1, arr2, arr3);
+
+            if (commonItems.Length == 0)
+            {
+                Console.WriteLine("공통 항목이 없습니다.");
+            }
+            else
+            {
+                Console.WriteLine("공통 항목:");
+                for (int i = 0; i < commonItems.Length; i++)
+                {
+                    Console.WriteLine(commonItems[i]);
+                }
+            }
         }
+
 
         static int SumOfDigits(int num)
         {
@@ -87,5 +140,37 @@
 
 
         }
+
+
+        // 11번 함수
+
+        static int[] FindCommonItems(int[] arr1, int[] arr2, int[] arr3)    // 정렬된 배열 셋에서 공통 항목을 찾는 메서드
+            
+        {
+            List<int> commonItems = new List<int>();        // 공통항목을 저장할 List 선언
+
+
+            // arr1 배열을 기준으로 각 항목이 나머지 배열에도 있는지 확인
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                int currentItem = arr1[i];      // 현재 항목
+
+                // 다른 배열에서도 현재 항목이 있는지 확인
+                // Indexof() 메서드를 사용해 배열에서 해당 항목의 인덱스를 찾음
+                // 인덱스를 찾을 수 없을 경우 -1을 반환하므로 0이상인 경우에만 해당 항목이 존재하는 것으로 판단
+                if (Array.IndexOf(arr2, currentItem) >= 0 &&
+                    Array.IndexOf(arr3, currentItem) >= 0)
+                {
+                    // 중복되지 않게 추가
+                    if (!commonItems.Contains(currentItem))
+                    {
+                        commonItems.Add(currentItem);
+                    }
+                }
+            }
+            // List를 배열로 변환하여 반환
+            return commonItems.ToArray();
+        }
+
     }
 }
