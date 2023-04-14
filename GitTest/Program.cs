a2db2a7003e70455f4db186c22b9724d4771996f
윤진 소 <pms010506@gmail.com>
2023-04-14 20:09:13 +0900

    Feat: 주어진 숫자가 소수인지 판별하는 솔루션

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -20,8 +20,48 @@
             Console.WriteLine("문장을 입력하세요. : ");
             string sentence = Console.ReadLine();
             string[] word = sentence.Split(' ');	// 공백을 기준으로 쪼갠다
-            Console.WriteLine(word.Length);		// 쪼갠 단어의 길이(개수)를 출력
+            Console.WriteLine(word.Length);     // 쪼갠 단어의 길이(개수)를 출력
 
+
+            // 9번
+
+            int num = 0;
+            bool deci = true;
+
+            Console.WriteLine("숫자를 입력하시오 : ");
+            num = int.Parse(Console.ReadLine());        // 형변환
+
+            if (num == 2)        // 입력값이 2일때 true
+            {
+                deci = true;
+            }
+
+            else if (num % 2 == 0)     // 짝수면 false
+            {
+                deci = false;
+            }
+
+            else
+            {
+                for (int i = 3; i < num; i++)     // num을 i=3부터 나누는 판별식
+                {
+                    if (num % i == 0)              // 나눈 나머지가 0일 경우
+                    {
+                        deci = false;
+                        break;                  // 바로 반복 중지
+                    }
+                    else
+                    {
+                        deci = true;
+                    }
+                }
+            }
+            if (deci == true)
+                Console.WriteLine(num + "은 소수입니다.");
+            else
+            {
+                Console.WriteLine(num + "은 소수가 아닙니다.");
+            }
 
         }
     }
