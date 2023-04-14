a4c4f68b0a2a3a3e35aca8ed0b0f70d607d649f7
윤진 소 <pms010506@gmail.com>
2023-04-14 20:04:52 +0900

    Feat: 7번 문자열 속 문자 찾기

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 7번 IndexOf() 사용
+
+            Console.WriteLine("문장을 입력하세요. : ");
+            string yummy = Console.ReadLine();
+            Console.WriteLine("단어를 입력하세요 : ");
+            string fruit = Console.ReadLine();
+
+            int a = yummy.IndexOf(fruit);
+
+            Console.WriteLine(a);
+
         }
     }
 }
