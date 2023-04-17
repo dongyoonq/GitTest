a3251d925d2b32a73a7300c7fd08b7b109b8b516
동윤 김 <ut_community@naver.com>
2023-04-17 17:58:45 +0900

    feat : 플레이어 아이템 사용 기능 추가

--- GitTest/Player.cs
+++ GitTest/Player.cs
@@ -13,5 +13,11 @@ namespace GitTest
         private string name;
 
         // 플레이어 개발 시작
+
+        public void UseItem(Item item)
+        {
+            // Item 클래스를 가져와 Player 클래스에서 사용
+            Console.WriteLine($"{name}이 아이템 사용.");
+        }
     }
 }
