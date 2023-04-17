801c1c4fda1ddca4ff42d8b5fade3c230497ac50
윤진 소 <pms010506@gmail.com>
2023-04-17 17:41:42 +0900

    Feat: 상어밥 스탯 추가

--- GitTest/SharkFood.cs
+++ GitTest/SharkFood.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace GitTest
 {
     internal class SharkFood
+        // 열라 강한 상어밥
     {
+        private int hp = 100000;
+        private int ap = 200000000;
     }
 }
