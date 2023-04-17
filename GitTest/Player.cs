76d7c013de62a2aa2d82d274f26a9c9e62fb69fe
동윤 김 <ut_community@naver.com>
2023-04-17 17:41:12 +0900

    feat : Player 개발 시작

---
+++ GitTest/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    internal class Player
+    {
+        private int hp;
+        private int mp;
+        private string name;
+
+        // 플레이어 개발 시작
+    }
+}
