764e2ef080eaa9134788c6381e949f1d5004c5f6
윤진 소 <pms010506@gmail.com>
2023-04-17 17:52:48 +0900

    Feat: 상돌이 피격반응 추가

--- GitTest/SharkFood.cs
+++ GitTest/SharkFood.cs
@@ -11,5 +11,11 @@ namespace GitTest
     {
         private int hp = 100000;
         private int ap = 200000000;
+
+        public void TakeDamage(int damage)
+        {
+            Console.WriteLine("상돌이가 데미지를 받음");
+            Console.WriteLine("상돌이가 울부짖는다");
+        }
     }
 }
